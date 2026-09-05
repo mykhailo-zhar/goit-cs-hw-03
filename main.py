540de786eb1e0472d8d1454aff179618568ea24b
Mykhailo Zhar <mykhailo.zhar.od.ua@gmail.com>
2026-09-05 15:31:54 +0300

    Add packages to main

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from src.mongo import main as mongo_main
+from src.queries import main as postgres_main
+
+
 def main():
-    print("Hello from TODO: replace goit-pycore-template!")
+    print("Queries to postgres: ")
+    postgres_main()
+    print("-" * 40)
+    print("Queries to Mongo:")
+    mongo_main()
 
 
 if __name__ == "__main__":
